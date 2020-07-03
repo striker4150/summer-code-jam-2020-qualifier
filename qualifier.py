82afe0abbf3f5e27fd169102eb1d115244d88992
Darren Kam <darrenmkam4150@gmail.com>
2020-07-03 01:07:50 -1000

    Complete qualifier challenges

--- qualifier.py
+++ qualifier.py
@@ -14,6 +14,7 @@ Important notes for submission:
   solution.
 """
 import datetime
+import re
 import typing
 
 
@@ -21,11 +22,69 @@ class ArticleField:
     """The `ArticleField` class for the Advanced Requirements."""
 
     def __init__(self, field_type: typing.Type[typing.Any]):
-        pass
+        self.values = {}
+        self.field_type = field_type
+
+    # Store the name of the set attribute
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    # Use instance ids to separate values
+    def __get__(self, instance, owner):
+        return self.values[id(instance)]
+
+    def __set__(self, instance, value):
+        if isinstance(value, self.field_type):
+            self.values[id(instance)] = value
+        else:
+            raise TypeError('expected an instance of type {} for attribute {}, got {} instead'.format(repr(self.field_type.__name__), repr(self.name), repr(type(value).__name__)))
 
 
 class Article:
     """The `Article` class you need to write for the qualifier."""
 
+    id = 0
+
     def __init__(self, title: str, author: str, publication_date: datetime.datetime, content: str):
-        pass
+        self.id = Article.id
+        Article.id += 1
+
+        self.title = title
+        self.author = author
+        self.publication_date = publication_date
+        self.content = content
+        self.last_edited = None
+
+    def __repr__(self):
+        args = (repr(self.title), repr(self.author), repr(self.publication_date.isoformat()))
+        return "<Article title={} author={} publication_date={}>".format(*args)
+
+    def __len__(self):
+        return len(self.content)
+
+    def __setattr__(self, name, value):
+        object.__setattr__(self, name, value)
+        if name == "content":
+            self.last_edited = datetime.datetime.now()
+
+    # Sort functions use __lt__(self, other)
+    def __lt__(self, other):
+        return self.publication_date < other.publication_date
+
+    def short_introduction(self, n_characters: int):
+        cutoff = n_characters
+        while self.content[cutoff] != " " and self.content[cutoff] != "\n":
+            cutoff = cutoff - 1
+        return self.content[0:cutoff]
+
+    def most_common_words(self, n_words: int):
+        words = re.findall('[a-z0-9]+', self.content.lower())
+        wordcount = {}
+        for word in words:
+            # Updating an OrderedDict does not change order
+            if word not in wordcount:
+                wordcount[word] = 1
+            else:
+                wordcount[word] = wordcount[word] + 1
+        # sorted() is guaranteed to be stable
+        return dict(sorted(wordcount.items(), key=lambda x: x[1], reverse=True)[0:n_words])
